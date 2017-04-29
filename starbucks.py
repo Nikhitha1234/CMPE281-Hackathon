f1a497424055e2f48ff72b52f4a8d81cb2363045
Nikhitha1234 <Nikhitha.ksr71@gmail.com>
2017-04-29 01:09:32 -0700

    Minor updates

--- starbucks.py
+++ starbucks.py
@@ -15,7 +15,6 @@ from random import randint
 from flask.globals import request
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
    return 'Welcome to Starbucks'
@@ -24,24 +23,23 @@ class Status:
         self.status = status
         self.message = message
 try:
-  #Connects to Mongo DB
   app.config['MONGO_DBNAME'] = 'restdb'
   app.config['MONGO_URI'] = 'mongodb://54.183.242.172:27017/restdb'
+  #app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
   uri = 'mongodb://54.183.242.172:27017/Starbucks'
+  #uri = 'mongodb://localhost:27017/Starbucks'
   mongo = MongoClient(uri)
-except pymongo.errors.ConnectionFailure as e:
+except ConnectionFailure as e:
     result = {}
     result['success'] = False
     result['error'] = "Db connection Failed"
     print(e)
 
-#Posts order to DB    
 @app.route('/api/PaloAlto/order/', methods=['POST'])
 def post_order():
   try:
-    starbucks = mongo.db.orders1
+    starbucks = mongo.db.testt
     maxi_id = 0
-    #Generates order id
     for s in starbucks.find():
         id = s['order_id']
         if id > maxi_id:
@@ -53,50 +51,49 @@ def post_order():
     milk = request.json['milk']
     size = request.json['size']
     object_id = starbucks.insert({'order_id':order_id,'location': location, 'qty': qty,'name':name,'milk':milk,'size':size})
-    text = 'Your Order Id is ' + str(order_id)
-    return(text)
-  except pymongo.errors.ConnectionFailure as e:
+    #text = 'Your Order Id is ' + str(order_id)
+    return str(order_id)
+  except ConnectionFailure as e:
     result = {}
     result['success'] = False
     result['error'] = "Db connection Failed"
     print(e)
 
-#Gets order details
+
 @app.route('/api/PaloAlto/order/', methods=['GET'])
-def get_all_stars():
-  starbucks = mongo.db.orders1
+def get_all_orders():
+  starbucks = mongo.db.testt
   output = []
   for s in starbucks.find():
-    output.append({'order_id':s['order_id'],'location': s['location'], 'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
-  return jsonify({'result' : output})
-#Gets orders based on order id
+    output.append({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
+  return jsonify(output)
+
 @app.route('/api/PaloAlto/order/<int:order_id>/', methods=['GET'])
-def get_one_star(order_id):
-  starbucks = mongo.db.orders1
+def get_one_order(order_id):
+  starbucks = mongo.db.testt
   output = []
   for s in starbucks.find({"order_id":order_id}):
-    output.append({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
-  return jsonify({'result' : output})
+    return jsonify({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
+  #return jsonify({'result' : output})
 
-#Updates orders using order id
 @app.route('/api/PaloAlto/order/<int:order_id>/', methods=['PUT'])
-def put_star(order_id):
-  starbucks = mongo.db.orders1
+def put_order(order_id):
+  starbucks = mongo.db.testt
   output = []
   location = request.json['location']
   qty = request.json['qty']
   name = request.json['name']
   milk = request.json['milk']
   size = request.json['size']
-  star_id = starbucks.update({'order_id':order_id},{'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
-  return "success"
+  star_id = starbucks.update({'order_id':order_id},{'order_id':order_id,'location': location,'qty': qty,'name':name,'milk':milk,'size':size})
+  for s in starbucks.find({"order_id":order_id}):
+    return jsonify({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
+  #return jsonify()
 
-#Deletes order based on order id
 @app.route('/api/PaloAlto/order/<int:order_id>/', methods=['DELETE'])
-def delete_star(order_id):
-  starbucks = mongo.db.orders1
+def delete_order(order_id):
+  starbucks = mongo.db.testt
   starbucks.remove({"order_id":order_id})
   return "success"
 if __name__ == '__main__':
-    #Can send requests from anywhere
-    app.run('0.0.0.0')
+   app.run('0.0.0.0'
