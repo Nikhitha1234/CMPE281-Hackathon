3a929e44983391f01364f8da39e981bf5f10eb5d
Nikhitha1234 <Nikhitha.ksr71@gmail.com>
2017-05-13 07:33:33 -0700

    Minor Changes

--- starbucks.py
+++ starbucks.py
@@ -12,14 +12,14 @@ app = Flask(__name__)
 def welcome():
    return 'Welcome to Starbucks'
 #Coapp.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONGO_URI'] = 'mongodb://54.183.242.172:27017/restdb'
-uri = 'mongodb://54.183.242.172:27017/Starbucks'
+app.config['MONGO_URI'] = 'mongodb://54.175.140.152:27017/'
+uri = 'mongodb://54.175.140.152:27017/StarBucksOrders'
 mongo = MongoClient(uri)
 
 #Route for POST request
 @app.route('/api/PaloAlto/order/', methods=['POST'])
 def post_order():
-  starbucks = mongo.db.testt
+  starbucks = mongo.db.StarBucksOrders
   maxi_id = 0
   for s in starbucks.find():
     id = s['order_id']
@@ -37,24 +37,24 @@ def post_order():
 #Route for GET request for all orders
 @app.route('/api/PaloAlto/order/', methods=['GET'])
 def get_all_orders():
-  starbucks = mongo.db.testt
+  starbucks = mongo.db.StarBucksOrders
   output = []
   for s in starbucks.find():
     output.append({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
   return jsonify(output)
 
 #Route for GET request based on order ID
-@app.route('/api/PaloAlto/order/<int:order_id>/', methods=['GET'])
+@app.route('/api/PaloAlto/order/<int:order_id>', methods=['GET'])
 def get_one_order(order_id):
-  starbucks = mongo.db.testt
+  starbucks = mongo.db.StarBucksOrders
   output = []
   for s in starbucks.find({"order_id":order_id}):
     return jsonify({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
 
 #Route for PUT request based on order ID
-@app.route('/api/PaloAlto/order/<int:order_id>/', methods=['PUT'])
+@app.route('/api/PaloAlto/order/<int:order_id>', methods=['PUT'])
 def put_order(order_id):
-  starbucks = mongo.db.testt
+  starbucks = mongo.db.StarBucksOrders
   output = []
   location = request.json['location']
   qty = request.json['qty']
@@ -67,11 +67,12 @@ def put_order(order_id):
     return jsonify({'order_id':s['order_id'],'location': s['location'],'qty': s['qty'],'name':s['name'],'milk':s['milk'],'size':s['size']})
 
 #Route for DELETE request based on order ID
-@app.route('/api/PaloAlto/order/<int:order_id>/', methods=['DELETE'])
+@app.route('/api/PaloAlto/order/<int:order_id>', methods=['DELETE'])
 def delete_order(order_id):
-  starbucks = mongo.db.testt
+  starbucks = mongo.db.StarBucksOrders
   starbucks.remove({"order_id":order_id})
   return "success"
 
 if __name__ == '__main__':
    app.run('0.0.0.0')
+                       
